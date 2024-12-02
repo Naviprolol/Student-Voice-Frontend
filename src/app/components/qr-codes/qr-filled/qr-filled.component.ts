c75bd058213c7f1fbb55dbeb1f36ebfd17b5fccd
vadim.r <roshchinvadim2003@gmail.com>
2024-12-02 23:51:53 +0500

    привязка к бэку, страница создания пары

---
+++ src/app/components/qr-codes/qr-filled/qr-filled.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-filled',
+  standalone: true,
+  imports: [],
+  templateUrl: './qr-filled.component.html',
+  styleUrl: './qr-filled.component.css'
+})
+export class QrFilledComponent {
+
+}
