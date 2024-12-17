5389551abbe230e63f6cbce56c4ebe4fb0f7f717
vadim.r <roshchinvadim2003@gmail.com>
2024-12-17 22:12:52 +0500

    вернул обратно

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://213.189.217.151:8000/api'
-};
--- src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://213.189.217.151:8000/api'
-};
